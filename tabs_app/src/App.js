77b1fc9afdf5edaf34364dbd0b79cc267656579b
BabiychukYuliya <julia4233097@gmail.com>
2023-10-26 15:46:56 +0300

    tabs add

--- tabs_app/src/App.js
+++ tabs_app/src/App.js
@@ -1,11 +1,27 @@
 import "./App.css";
+import { useEffect, useState } from "react";
 import Tabs from "./components/tabs";
+import request from "./components/utils/request";
 
 export const App = ({ routes }) => {
+  const [tabs, setTabs] = useState([]);
+
+  const fetchTabs = async () => {
+    const { result, error } = await request("tabs.json");
+    setTabs(result);
+    if (error) {
+      return;
+    }
+  };
+
+  useEffect(() => {
+    fetchTabs();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header"></header>
-      <Tabs />
+      <Tabs tabs={tabs} />
     </div>
   );
 };
--- tabs_app/src/components/tabs/index.js
+++ tabs_app/src/components/tabs/index.js
@@ -1,3 +1,26 @@
+import { NavLink } from "react-router-dom";
+
 const HOME_URL = "/";
 const isActiveTab = (id, key, path) =>
   path === `${HOME_URL}${id}` || (path === `${HOME_URL}` && !key);
+
+const Tabs = ({ tabs, path }) => {
+  return (
+    <nav>
+      <ul>
+        {tabs.map(({ id, title }, key) => (
+          <li key={id}>
+            <NavLink
+              isActive={isActiveTab(id, key, path)}
+              to={`${HOME_URL}${id}`}
+            >
+              {title}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Tabs;
